d861477ddac429737b07633218b026f6a44e3a8c
eliiasg <egrundtvig@gmail.com>
2024-09-08 13:29:54 +0200

    add binsearch

--- crates/modula_utils/src/lib.rs
+++ crates/modula_utils/src/lib.rs
@@ -1,13 +1,15 @@
+use std::ops::Range;
+
 use bevy_ecs::prelude::*;
 pub use hashbrown;
-use modula_core::{EventOccured, EventRes, ScheduleBuilder, ShuoldExit};
+use modula_core::{EventOccurred, EventRes, ScheduleBuilder, ShuoldExit};
 use winit::event::{Event, WindowEvent};
 
 pub type HashMap<K, V> = hashbrown::HashMap<K, V>;
 pub type HashSet<T> = hashbrown::HashSet<T>;
 
 pub fn init_window_closing(schedule_builder: &mut ScheduleBuilder) {
-    schedule_builder.add_systems(EventOccured, handle_window_close)
+    schedule_builder.add_systems(EventOccurred, handle_window_close)
 }
 
 fn handle_window_close(mut commands: Commands, event: Res<EventRes>) {
@@ -18,4 +20,25 @@ fn handle_window_close(mut commands: Commands, event: Res<EventRes>) {
         } => commands.insert_resource(ShuoldExit),
         _ => {}
     }
+}
+
+/// Binary searches between lower and upper, returning the lowest value giving ok, if all values give error, the error returned by the end of the range is returned
+pub fn binsearch<T, E>(
+    mut f: impl FnMut(i32) -> Result<T, E>,
+    mut range: Range<i32>,
+) -> Result<T, E> {
+    if range.is_empty() {
+        panic!("binsearch on empty range");
+    }
+    let mut res = None;
+    while range.start < range.end {
+        let mid = (range.start + range.end) / 2;
+        res = Some(f(mid));
+        if res.as_ref().unwrap().is_ok() {
+            range.end = mid;
+        } else {
+            range.start = mid + 1;
+        }
+    }
+    res.unwrap()
 }
